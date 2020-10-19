4de56e6656a68f15ea2146898a4af2c3cdedc822
sshashwatkashyap <ssuchitrasingh@gmail.com>
2020-10-19 20:55:05 +0530

    trajectory

--- sketch.js
+++ sketch.js
@@ -7,6 +7,8 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var GameState = "ONSLING";
+
 
 
 function preload() {
@@ -69,16 +71,25 @@ function draw(){
 }
 
 function mouseDragged(){
+
+    if(GameState !== "LAUNCH"){
+
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+
+    }
 }
 
 
 function mouseReleased(){
-    slingshot.fly();
+  
+  GameState = "LAUNCH"
+
+  slingshot.fly();
+
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+//        slingshot.attach(bird.body);
     }
 }
